49563fd527f6cc456287b02ef731cb71041e596a
kaiyu li <krisfk@gmail.com>
2021-09-20 05:17:36 +0800

    .

--- wp-content/themes/wonderland/woocommerce/archive-product.php
+++ wp-content/themes/wonderland/woocommerce/archive-product.php
@@ -251,7 +251,7 @@ for($i=0;$i<8;$i++)
     <?php 
         echo paginate_links( array(
             'base'         => '%_%',
-            str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            // str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
             'total'        => $query->max_num_pages,
             'current'      => max( 1, get_query_var( 'page' ) ),
             'format'       => '?page=%#%',
