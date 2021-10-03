f81e1d6d3bdd818c105ede9d045c8f0873fa28f4
kaiyu li <krisfk@gmail.com>
2021-10-04 04:15:15 +0800

    .

--- wp-content/themes/wonderland/header.php
+++ wp-content/themes/wonderland/header.php
@@ -88,6 +88,11 @@
 
     <div class="warn-lightbox">
         <div class="container">
+            <!-- <a href="<?php echo get_site_url();?>" class="logo-a col-2"> -->
+            <img src="<?php echo get_template_directory_uri()?>/assets/images/logo-clean.png" alt="">
+            <!-- </a> -->
+            <br><br>
+
             警告: <br>
             本網頁內容及相關物品可能令人反感<br>
             不可將其內容及物品派發、傳閱、出售、出租、交給或出借予年齡未滿18歲的人士或將其內容及物品向該等人士出示、播凡年齡未滿十八或當地政府規定的合法年齡 !<br>
